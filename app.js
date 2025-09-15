32e32dbf342a5c300c59d7e7b560ddf62489bf80
jbena22 <jhonattangbc@gmail.com>
2025-09-14 19:33:51 -0500

    Implementa una función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ Versión: 1.0
 let amigos = [];
 //Variable para capturar nuevo nombre
 let nombre;
+//Variable para capturar nuevo elemento de la lista
+let lista;
 
 function agregarAmigo(){
     //.trim() elimina espacios. 
@@ -17,19 +19,32 @@ function agregarAmigo(){
     nombre = document.getElementById("amigo").value.trim().toUpperCase();
     let soloLetras = /^[A-Za-zÁÉÍÓÚáéíóúñÑ]+$/; //Permite letras con mínimo 2 caracteres
 
-    // Valida que el nombre:
-    // 1. No esté vacío (longitud mayor a 1 carácter).
-    // 2. Cumpla con la expresión regular.
-    // 3. No exista ya en el array.
-    // Si todo es correcto, se actualiza el array de amigos;
-    // de lo contrario, se muestra un mensaje de alerta.
+    //Valida que el nombre:
+    //1. No esté vacío (longitud mayor a 1 carácter).
+    //2. Cumpla con la expresión regular.
+    //3. No exista ya en el array.
+    //Si todo es correcto, se actualiza el array de amigos y llama a la función actualizarListaAmigos();
+    //de lo contrario, se muestra un mensaje de alerta.
     if ( nombre.length >= 1 && soloLetras.test(nombre) && !amigos.includes(nombre)){       
         amigos.push(nombre);
-        console.log(amigos);
+        actualizarListaAmigos();
     } else{
         alert("Por favor, inserte un nombre. Además, no debe repetirse."); 
     }  
     
     //Limpia el campo
     document.getElementById("amigo").value = "";
+}
+
+function actualizarListaAmigos(){
+    lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; //Limpia la lista existente
+
+    //Recorre el array de amigos y crea un elemento <li> por cada uno,
+    //agregándolo a la lista
+    for (let i = 0; i < amigos.length; i++){
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
 }
